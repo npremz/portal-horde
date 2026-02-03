bbeca48438257e82bca9cf6d040813f619b6301d
npremont <ni.premont@gmail.com>
2026-02-03 11:38:23 +0100

    E2E fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,12 +30,10 @@ export default defineConfig({
       use: { ...devices["Pixel 5"] },
     },
   ],
-  webServer: process.env.CI
-    ? undefined
-    : {
-        command: "npm run dev",
-        url: "http://localhost:3000",
-        reuseExistingServer: !process.env.CI,
-        timeout: 120000,
-      },
+  webServer: {
+    command: process.env.CI ? "npm run start" : "npm run dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
 });
